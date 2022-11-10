4b2aee2212f0af283e08d4b0ad352072e722dbdd
chungvux <vuchung249@gmail.com>
2022-11-10 22:19:39 +0700

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+export const Postgresql = {
+  host: '103.184.113.163',
+  username: 'chung',
+  password: 'chung123456',
+  dialect: 'postgres',
+  port: 5432,
+};
+export const Mysql = {
+  host: '103.184.113.163',
+  username: 'chung',
+  password: 'chung123456',
+  dialect: 'mysql',
+  port: 3306,
+}
+
+export const MongoDB = {
+  uri: `mongodb+srv://chung:1234@cluster0.plitd.mongodb.net/?retryWrites=true&w=majority`
+}
